3a0fdc4345bd73d1d1f4e7ea3f6924a2cc2efb0a
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-10-05 09:23:12 +0200

    generate api

--- src/tools/oapi.go
+++ src/tools/oapi.go
@@ -1,8 +1,8 @@
 //go:build tools
 // +build tools
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=types.cfg.yaml ../../api/api.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=server.cfg.yaml ../../api/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=../../api/types.cfg.yaml ../../api/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=../../api/server.cfg.yaml ../../api/api.yaml
 
 package tools
 

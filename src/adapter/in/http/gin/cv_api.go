65df68be76fc3394fcfe74241e9f02ed7eeebcef
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-10-18 14:45:24 +0200

    api to in/adapter

---
+++ src/adapter/in/http/gin/cv_api.go
@@ -0,0 +1,11 @@
+//go:build tools
+// +build tools
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=./types.cfg.yaml ../../../../../api/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=./server.cfg.yaml ../../../../../api/api.yaml
+
+package api
+
+import (
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+)

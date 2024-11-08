1d8a17929fca9252833ad02cab22a8ee2d40db08
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-11-08 12:26:20 +0100

    Split into controller and api

--- adapter/in/http/cv/cv_api.go
+++ adapter/in/http/cv/cv_api.go
@@ -1,7 +1,6 @@
 package cv
 
 import (
-	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
@@ -9,122 +8,12 @@ import (
 
 	"log/slog"
 
-	"github.com/ernstvorsteveld/go-cv-cassandra/domain/port/in"
 	m "github.com/ernstvorsteveld/go-cv-cassandra/pkg/middleware"
 	"github.com/ernstvorsteveld/go-cv-cassandra/pkg/utils"
 	"github.com/gin-gonic/gin"
 
 	middleware "github.com/oapi-codegen/gin-middleware"
 )
-
-const OBJECT_ID_HEADER = "X-OBJECT-ID"
-const LOCATION_HEADER = "Location"
-
-type CvApiHandler struct {
-	u in.UseCasesPort
-	c *utils.Configuration
-}
-
-func NewCvApiService(u in.UseCasesPort, c *utils.Configuration) *CvApiHandler {
-	return &CvApiHandler{
-		u: u,
-		c: c,
-	}
-}
-
-func (cs *CvApiHandler) ListExperiences(c *gin.Context, params ListExperiencesParams) {
-	cId := m.GetCorrelationIdHeader(c)
-	slog.Debug("cv.ListExperiences", "content", "About to List Experiences", "correlationId", cId)
-	ctx := utils.NewDefaultContextWrapper(c, cId).Build()
-	es, err := cs.u.ListExperiences(ctx, in.NewListExperienceCommand(int(*params.Page), int(*params.Limit)))
-	if err != nil {
-		NewListExperienceError(c, err)
-		return
-	}
-	_, err = json.Marshal(es)
-	if err != nil {
-		NewListExperienceMarshalError(c, err)
-		return
-	}
-
-	c.JSON(http.StatusOK, es)
-}
-
-// Create an experience
-// (POST /experiences)
-func (cs *CvApiHandler) CreateExperience(c *gin.Context) {
-	cId := m.GetCorrelationIdHeader(c)
-	slog.Debug("cv.CreateExperience", "content", "About to Create an Experience", "correlationId", cId)
-	ctx := utils.NewDefaultContextWrapper(c, cId).AddUrl(cs.c.Api.CV.Url).Build()
-
-	var e CreateExperienceRequest
-	if err := c.ShouldBindJSON(&e); err != nil {
-		NewExperienceBindError(c, err)
-		return
-	}
-	m, err := cs.u.CreateExperience(ctx, in.NewCreateExperienceCommand(e.Name, e.Tags))
-	if err != nil {
-		NewCreateExperienceError(c, err)
-		return
-	}
-	c.Writer.Header().Set(LOCATION_HEADER, fmt.Sprintf("%s/experiences/%s", utils.GetHostUrl(ctx), m.GetId()))
-	c.Writer.Header().Set(OBJECT_ID_HEADER, m.Id)
-
-	c.Data(http.StatusCreated, "application/json", []byte(m.Id))
-}
-
-// Info for a specific experience
-// (GET /experiences/{id})
-func (cs *CvApiHandler) GetExperienceById(c *gin.Context, id string) {
-	cId := m.GetCorrelationIdHeader(c)
-	slog.Debug("cv.GetExperienceById", "content", "About to Get Experience by Id", "correlationId", cId)
-	ctx := utils.NewDefaultContextWrapper(c, cId).Build()
-	e, err := cs.u.GetExperienceById(ctx, in.NewGetExperienceCommand(id))
-	if err == nil {
-		_, err := json.Marshal(e)
-		if err != nil {
-			NewGetExperienceByIdMarshalError(c, err)
-			return
-		}
-		c.JSON(http.StatusOK, e)
-	} else {
-		NewGetExperienceByIdNotFoundError(c, err)
-	}
-}
-
-func (cs *CvApiHandler) ListTags(c *gin.Context) {
-	cId := m.GetCorrelationIdHeader(c)
-	slog.Debug("cv.ListTags", "content", "About to List Tags, using defaults page=0 and size=100")
-	ctx := utils.NewDefaultContextWrapper(c, cId).Build()
-	tags, err := cs.u.ListTags(ctx, in.NewListTagsCommand(int(0), int(100)))
-	if err != nil {
-		NewListTagsError(c, err)
-		return
-	}
-	c.JSON(http.StatusOK, tags)
-}
-
-// Create a tag
-// (POST /tags)
-func (cs *CvApiHandler) CreateTag(c *gin.Context) {
-	cId := m.GetCorrelationIdHeader(c)
-	slog.Debug("cv.CreateTag", "content", "About to create Tag")
-	ctx := utils.NewDefaultContextWrapper(c, cId).AddUrl(cs.c.Api.CV.Url).Build()
-
-	var t Tag
-	if err := c.ShouldBindJSON(&t); err != nil {
-		NewCreateTagMarshalError(c, err)
-		return
-	}
-	m, err := cs.u.CreateTag(ctx, in.NewCreateTagCommand(t.Tag))
-	if err != nil {
-		NewCreateTagError(c, err)
-		return
-	}
-	c.Writer.Header().Set(LOCATION_HEADER, fmt.Sprintf("%s/tags/%s", utils.GetHostUrl(ctx), m.GetId()))
-	c.Writer.Header().Set(OBJECT_ID_HEADER, m.Id)
-	c.JSON(http.StatusCreated, m.Id)
-}
 
 func NewGinCvServer(h *CvApiHandler, c *utils.Configuration) *http.Server {
 	slog.Debug("NewGinCvServer", "content", "About to create GinCVServer", "port", c.Api.CV.Port)
